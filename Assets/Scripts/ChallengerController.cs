353c996e293f1fe2db388d05a51ef11846f307ad
Aliaksei Banshchyk <alexban04@gmail.com>
2025-05-21 17:58:28 +0300

    Added an empty battle scene. Added transition to battle scene. Added tiles and layer for floor that can trigger a battle randomly.

--- Assets/Scripts/ChallengerController.cs
+++ Assets/Scripts/ChallengerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChallengerController : MonoBehaviour, Interactable
 {
     public void Interact()
     {
         Debug.Log("Battle Start");
+        SceneManager.LoadScene(1);
     }
     
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using Unity.Multiplayer.Center.Common.Analytics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float acceleration;
+    [SerializeField] float maxBattleChance;
+    float battleChance;
+    [SerializeField] float battleChanceGrowth;
 
     private float currentSpeed;
     private UnityEngine.Vector3 input;
@@ -15,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     public UnityEngine.LayerMask solidObjectsLayer;
     public UnityEngine.LayerMask interactablesLayer;
+    public UnityEngine.LayerMask battleLayer;
 
     private void Awake()
     {
@@ -39,6 +44,7 @@ public class PlayerController : MonoBehaviour
             lastInput = input;
             currentSpeed += acceleration;
             isMoving = true;
+            checkForEncounters();
         }
         else
         {
@@ -46,8 +52,13 @@ public class PlayerController : MonoBehaviour
             isMoving = false;
         }
         currentSpeed = Mathf.Clamp(currentSpeed, 0, moveSpeed);
+        if (currentSpeed != 0)
+        {
+            battleChance += battleChanceGrowth;
+            battleChance = Mathf.Clamp(battleChance, 0, maxBattleChance);
+        }
         if (isWalkable(transform.position + input * currentSpeed))
-            transform.position += input * currentSpeed;
+                transform.position += input * currentSpeed;
         if (input.x > 0 && !facingRight || input.x < 0 && facingRight)
         {
             UnityEngine.Vector2 playerScale = transform.localScale;
@@ -67,14 +78,22 @@ public class PlayerController : MonoBehaviour
     }
     void Interact()
     {
-        Debug.Log("E is pressed");
-        var interactPos = transform.position + lastInput * moveSpeed;
-        Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
-
-        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactablesLayer);
+        var collider = Physics2D.OverlapCircle(transform.position, 1f, interactablesLayer);
         if (collider != null)
         {
             collider.GetComponent<Interactable>()?.Interact();
+        }
+    }
+    private void checkForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.01f, battleLayer) != null)
+        {
+            if (Random.Range(1, 100) <= battleChance)
+            {
+                Debug.Log("Start Battle");
+                SceneManager.LoadScene(1);
+                battleChance = 0;
+            }
         }
     }
 }
